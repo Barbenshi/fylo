a731531629506f500e2fefda3b7a6763057664a9
Bar Ben Shimol <barbenshimol2@gmail.com>
2022-12-14 16:49:07 +0200

    lol

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/mister-toy/',
+  base: '/fylo/',
   plugins: [vue()],
   server: {
     port: 3001
